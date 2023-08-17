78ae9f043a6e9a84997edfad7079e35337742cd9
ljq <greaclar@163.com>
2023-08-17 10:52:05 +0800

    refactor: 修改导出的WSEventsConst为WSEventsMap

--- src/socket-webox.ts
+++ src/socket-webox.ts
@@ -1,4 +1,4 @@
 import initSocketWebox, { SocketWebox as MySocket } from './webSocket/socket';
-export { type SocketWeboxType, type initWSOptionsType, type initHeartbeatOptionsType, WSEventsConst } from './webSocket/socket.type';
+export { type SocketWeboxType, type initWSOptionsType, type initHeartbeatOptionsType, WSEventsConst as WSEventsMap } from './webSocket/socket.type';
 export const SocketWebox = MySocket;
 export default initSocketWebox;
